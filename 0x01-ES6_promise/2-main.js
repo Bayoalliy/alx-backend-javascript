898bc15fefb0dfba89de07472278440f1fa768ab
bayoalliy <adebayoalliy@gmail.com>
2024-10-31 00:07:43 +0000

    all

--- 0x01-ES6_promise/2-main.js
+++
@@ -1,7 +0,0 @@
-import handleResponseFromAPI from "./2-then";
-
-const promise = Promise.resolve();
-console.log(promise);
-let a = handleResponseFromAPI(promise);
-console.log(a);
-console.log(promise);
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const create = createUser();
+  Promise.all([upload, create])
+    .then((results) => {
+      console.log(results[0].body, results[1].firstName, results[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
