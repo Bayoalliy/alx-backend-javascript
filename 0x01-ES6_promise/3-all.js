306013c56a05493ab04c6178fafa9ec43ff81f34
bayoalliy <adebayoalliy@gmail.com>
2024-10-31 00:31:23 +0000

    return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const upload = uploadPhoto();
-  const create = createUser();
-  return Promise.all([upload, create])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(results[0].body, results[1].firstName, results[1].lastName);
     })
