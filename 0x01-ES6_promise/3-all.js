35a417103d29a6886b652c23abc1311167b9e8a4
bayoalliy <adebayoalliy@gmail.com>
2024-10-31 22:51:46 +0000

    gg

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.allSettled([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
