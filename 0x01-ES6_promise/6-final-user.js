12fb4fc139d2b37c850c12da3df17112a99964ca
bayoalliy <adebayoalliy@gmail.com>
2024-12-04 22:02:30 +0000

    String()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res);
+    .then((res) => (
+      res.map((r) => ({
+        status: r.status,
+        value: r.status === 'rejected' ? String(r.reason) : r.value,
+      }))));
 }
