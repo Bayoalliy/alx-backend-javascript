4e721496634cf7f59f69654fa3ead34b44962636
adebayo <adebayoalliy@gmail.com>
2024-12-05 14:22:20 +0000

    yy

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  if denominator == 0:
-	try{
-          throw new Error('cannot divide by 0');
-	} catch((err) => err)
-  else:
-	return(numerator / denoominator)
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
+  }
 }
