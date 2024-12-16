a19520e1d1d2f5e1ba6cc369ae4b5a97ae7d8a01
adebayo <adebayoalliy@gmail.com>
2024-12-16 14:19:47 +0000

    map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,14 @@
 export default function getListStudentIds(objLst) {
   const lst = [];
-  for (const obj of objLst) {
+  function getId(obj) {
     for (const key in obj) {
       if (key === 'id') {
         lst.push(obj[key]);
       }
     }
+  }
+  if (Array.isArray(objLst)) {
+    objLst.map(getId);
   }
   return lst;
 }
