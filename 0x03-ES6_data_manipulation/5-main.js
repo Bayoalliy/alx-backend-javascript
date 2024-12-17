a2ca7b881c89f3bbedb13ea74fe3dca16c5f2b5e
adebayo <adebayoalliy@gmail.com>
2024-12-17 21:59:40 +0000

    data view

--- 0x03-ES6_data_manipulation/5-main.js
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -1,3 +1,6 @@
 import createInt8TypedArray from "./5-typed_arrays.js";
 
 console.log(createInt8TypedArray(10, 2, 89));
+
+let array = createInt8TypedArray(10, 0, 5);
+console.log(array.getInt8(0));
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,21 @@
+/*
+ * Create a function named createInt8TypedArray that returns a new
+ * ArrayBuffer with an Int8 value at a specific position.
+ *
+ * It should accept three arguments: length (Number),
+ * position (Number), and value (Number).
+ *
+ * If adding the value is not possible
+ * the error Position outside range should be thrown
+ */
+
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
-  return buffer;
+  const int8View = new DataView(buffer);
+  int8View.setInt8(position, value);
+  return int8View;
 }
