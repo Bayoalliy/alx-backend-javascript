a3e0b1724458292765402191c823fc1a00ad7adf
adebayo <adebayoalliy@gmail.com>
2024-12-17 22:41:47 +0000

    gg

--- 0x03-ES6_data_manipulation/7-main.js
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -3,4 +3,8 @@ import hasValuesFromArray from "./7-has_array_values.js";
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
-console.log(hasValuesFromArray(new Set([1, 2, 'test']), [1, 2, 'test', 'banana']))
+
+const array = [1, 2, 'test'];
+const set = new Set(array);
+const array2 = [1, 2, 'test', 'banana']
+console.log(hasValuesFromArray(set, array2));
