1acc3cae0dd0a190fc5e18a31734e915fd2be66a
adebayo <adebayoalliy@gmail.com>
2024-12-18 17:10:59 +0000

    hot fix

--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -4,3 +4,4 @@ console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
 const set = new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']);
 console.log(cleanSet(set, 'id-'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana'])));
